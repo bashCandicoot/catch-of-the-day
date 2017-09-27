0d713edb17ddcf72e13d5d4f539859a6338ec17d
steve <bashcandicoot@gmail.com>
2017-09-27 21:23:40 +0100

    added ref to store-selector form. goToStore console logs storeInput.value

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {getFunName} from '../helpers'
 
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+    goToStore(event) {
+        event.preventDefault();
+        console.log(this.storeInput.value)
+        // console.log(this.goToStore());
+        // first grab texzt from box
+        /// then transition the url to store/:storeId
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="store name"/>
+                <input type="text" required placeholder="store name"
+                defaultValue={getFunName()} 
+                ref={ (input) => {this.storeInput = input } } />
+
                 <button type="submit">visitin store</button>
             </form>
         )
