3569ec33ddf2138c008e8e4915c3099f7cf8d651
steve <bashcandicoot@gmail.com>
2017-09-27 19:39:32 +0100

    added not found route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router';
+
 import StorePicker from './components/StorePicker';
 import App from './components/App'
+import NotFound from './components/NotFound';
+
 import './css/style.css';
 
-render(<App/>, document.body)
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker}/>
+                <Match exactly pattern="/store/:storeId" component={App}/>
+                <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+render(
+    <Root/>, document.body)
