4a5e054e92f86dd5d7bacd31759b66e9804708e9
steve <bashcandicoot@gmail.com>
2017-09-30 16:30:26 +0100

    created App constructor to initialize a state which can hold our fish and order states. Created an addFish function to add a fish to our fish state object

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,28 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    constructor() {
+        super()
+        //  initialize state
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+        // bind addFish function to App component
+        this.addFish = this.addFish.bind(this);
+    }
+    
+    addFish(fish) {
+        // update our state
+        const fishes = {...this.state.fishes};
+        // add new fish
+        const timestamp = Date.now()
+        fishes[`fish-${timestamp}`] = fish;
+        // the fishes state has changed so we set it
+        // could also do { fishes } instad of { fishes: fishes }
+        this.setState({fishes: fishes});
+
+    }
 
     render() {
         return (
@@ -12,7 +34,7 @@ class App extends React.Component {
                     <Header tagline="Fresh afff"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }
